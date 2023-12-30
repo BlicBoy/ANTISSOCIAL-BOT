46db24756d705907ddbd593398c4ab04fdd100db
BlicBoy <g.silva.oliveira.2002@gmail.com>
2023-12-30 01:19:27 +0000

    Timout and helpers

--- commands/blackjack.js
+++ commands/blackjack.js
@@ -8,16 +8,31 @@ const { SlashCommandBuilder } = require("discord.js")
 const { createChannels } = require("./helpers/createChannel")
 const { dateFormater } = require("./helpers/other")
 
+var timeout = []
 
 module.exports = {
-  cooldown: 60, //seconds
   data:  new SlashCommandBuilder()
         .setName("blackjack")
         .setDescription("Casino game - BlackJack 🃏"),
   async execute(interaction){
-    var nameUser = interaction.member.user.username
-    let date = dateFormater()
-    await createChannels('blackjack', nameUser, date)
-    await interaction.reply("pong")
+    if(timeout.includes(interaction.user.id)){
+      console.log(`User ${interaction.user.username} está a spammar o bot`)
+      await interaction.reply({ content: ' 🛑 You are on cooldown, try again in 1 minute 🛑', ephemeral: true})
+      return
+    }
+    await initGame(interaction)
+
+    timeout.push(interaction.user.id)
+    setTimeout(() => {
+      timeout.shift()
+    }, 60000)
   }
+}
+
+
+async function initGame(interaction) {
+  var nameUser = interaction.user.username
+  let date = dateFormater()
+  await createChannels('blackjack', nameUser, date)
+  await interaction.reply("blackjack")
 }
--- commands/roulette.js
+++ commands/roulette.js
@@ -5,12 +5,33 @@
  */
 
 const { SlashCommandBuilder } = require("discord.js")
+const { createChannels } = require("./helpers/createChannel")
+const { dateFormater } = require("./helpers/other")
+
+var timeout = []
 
 module.exports = {
   data:  new SlashCommandBuilder()
         .setName("roullete")
         .setDescription("Casino game - Roulette 🎡"),
-  async execute(interaction){
-      await interaction.reply("pong")
-  }
+    async execute(interaction){
+      if(timeout.includes(interaction.user.id)){
+        console.log(`User ${interaction.user.username} está a spammar o bot`)
+        await interaction.reply({ content: ' 🛑 You are on cooldown, try again in 1 minute 🛑', ephemeral: true})
+        return
+      }
+      await initGame(interaction)
+
+      timeout.push(interaction.user.id)
+      setTimeout(() => {
+        timeout.shift()
+      }, 60000)
+    }
+}
+
+async function initGame(interaction) {
+  var nameUser = interaction.user.username
+  let date = dateFormater()
+  await createChannels('roullete', nameUser, date)
+  await interaction.reply("roullete")
 }
